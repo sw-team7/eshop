b900ee861f80b509ae9b6f99a5922bfe38202334
Shafayet Rahman <ncc.shad@gmail.com>
2018-11-10 23:34:02 +0600

    Product description and Product details modified

--- application/views/user/product_view.php
+++ application/views/user/product_view.php
@@ -79,24 +79,10 @@
 						</h3>
 						<span class="product-available">In Stock</span>
 					</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-						laboris nisi ut aliquip ex ea commodo consequat.</p>
 
-					<div class="product-options">
-						<label>
-							Size
-							<select class="input-select">
-								<option value="0">X</option>
-							</select>
-						</label>
-						<label>
-							Color
-							<select class="input-select">
-								<option value="0">Red</option>
-							</select>
-						</label>
-					</div>
+                    <?= $product->product_name; ?>
+
+
 
 					<div class="add-to-cart">
 						<div class="qty-label">
@@ -110,10 +96,7 @@
 						<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
 					</div>
 
-					<ul class="product-btns">
-						<li><a href="#"><i class="fa fa-heart-o"></i> add to wishlist</a></li>
-						<li><a href="#"><i class="fa fa-exchange"></i> add to compare</a></li>
-					</ul>
+
 
 					<ul class="product-links">
 						<li>Category:</li>
@@ -121,16 +104,12 @@
 						<li><a href="#"><?= $product->cat_name; ?></a></li>
 					</ul>
 
-					<ul class="product-links">
-						<li>Share:</li>
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-						<li><a href="#"><i class="fa fa-envelope"></i></a></li>
-					</ul>
 
 				</div>
 			</div>
+
+
+
 			<!-- /Product details -->
 
 			<!-- Product tab -->
@@ -150,8 +129,11 @@
 						<div id="tab1" class="tab-pane fade in active">
 							<div class="row">
 								<div class="col-md-12">
-									<p>Product Description</p>
-								</div>
+
+                                    <p>Here the description of the product will be showed</p><br>
+
+
+                                </div>
 							</div>
 						</div>
 						<!-- /tab1  -->
@@ -160,8 +142,12 @@
 						<div id="tab2" class="tab-pane fade in">
 							<div class="row">
 								<div class="col-md-12">
-									<p><?= $product->body; ?></p>
-								</div>
+                                    <h5> Product Name:  <?= $product->product_name; ?> </h5>
+                                    <h5> Category name: <?= $product->cat_name; ?></h5>
+                                    <h5> Product Price: <?= $product->price; ?> BDT</h5>
+
+
+                                </div>
 							</div>
 						</div>
 						<!-- /tab2  -->
